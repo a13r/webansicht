21fb45c636635ba071c4d897fdec118026fedbb3
Andreas Bruckner <andreas.bruckner@w.roteskreuz.at>
2018-08-11 16:39:19 +0200

    add updated at timestamp to station panel in overview

--- web/containers/overview.jsx
+++ web/containers/overview.jsx
@@ -5,6 +5,7 @@ import {Col, Panel, Row} from "react-bootstrap";
 import {inject, observer} from "mobx-react";
 import authenticate from "../components/authenticate";
 import StationLoad from "~/components/StationLoad";
+import moment from "moment";
 
 export default authenticate(inject('auth', 'stations')(observer(({auth, stations}) =>
     <Row>
@@ -14,7 +15,7 @@ export default authenticate(inject('auth', 'stations')(observer(({auth, stations
         <Col md={3}>
             {auth.isDispo && <ResourceEditor/>}
             {stations.list.map(s =>
-                <Panel key={s._id} header={s.name}>
+                <Panel key={s._id} header={<span>{s.name}<span className="pull-right"><small>seit {moment(s.updatedAt).format('HH:mm')}</small></span></span>}>
                     <StationLoad now={s.loadPercentage} label={s.loadLabel} style={{marginBottom: 0}}/>
                 </Panel>)}
         </Col>
