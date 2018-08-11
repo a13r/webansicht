cdd0d866ab8e7e5f6265462b798f4a2bcac111da
Andreas Bruckner <e0929234@student.tuwien.ac.at>
2018-08-11 22:02:25 +0200

    add total sum to transports page

--- web/containers/transports.jsx
+++ web/containers/transports.jsx
@@ -7,19 +7,22 @@ export default authenticate(inject('transports')(observer(({transports}) =>
     <div className="panel panel-default">
         <table className="table table-striped table-bordered">
             <thead>
-                <tr>
-                    <th style={{width: '10em'}}>Zeitpunkt</th>
-                    <th>Beschreibung</th>
-                    <th style={{width: '5em'}}>Status</th>
-                </tr>
+            <tr>
+                <th style={{width: '10em'}}>Zeitpunkt</th>
+                <th>Beschreibung</th>
+                <th style={{width: '5em'}}>Status</th>
+            </tr>
             </thead>
             <tbody>
-                {transports.list.map(t =>
-                    <tr key={t._id}>
-                        <td>{moment(t.createdAt).format('L LT')}</td>
-                        <td>{t.text}</td>
-                        <td>{t.state}</td>
-                    </tr>)}
+            {transports.list.map(t =>
+                <tr key={t._id}>
+                    <td>{moment(t.createdAt).format('L LT')}</td>
+                    <td>{t.text}</td>
+                    <td>{t.state}</td>
+                </tr>)}
             </tbody>
         </table>
+        <div className="panel-footer">
+            Gesamtanzahl: {transports.list.length}
+        </div>
     </div>)));
