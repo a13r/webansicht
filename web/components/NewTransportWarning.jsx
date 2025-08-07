753c83b5b0fc71df26dfcb554c78ef5ae732d1df
Andreas Bruckner <andreas@bruckner.cc>
2025-08-07 18:00:12 +0200

    chore: remove react-animation-components, optimize NewTransportWarning, and add fade animation via CSS

--- web/components/NewTransportWarning.jsx
+++ web/components/NewTransportWarning.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import {Fade, Loop} from 'react-animation-components';
 
 export const NewTransportWarning = () =>
-    <Loop in>
-        <Fade style={{display: 'inline'}}><i className="fa fa-lg fa-ambulance text-danger"/></Fade>
-    </Loop>;
+    <i className="fa fa-lg fa-ambulance text-danger new-transport-warning"/>;
