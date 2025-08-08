cf3be63f69fc07018aa6b2f8c2cbddd39bf33fcc
Andreas Bruckner <andreas@bruckner.cc>
2025-08-08 17:36:38 +0200

    fix: prevent duplicate log entries by comparing tracked properties

--- src/hooks/create-log-entry.js
+++ src/hooks/create-log-entry.js
@@ -1,11 +1,23 @@
 const _ = require('lodash');
+const equal = require("deep-equal");
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
-    return function(hook) {
-        const entry = Object.assign({}, hook.result);
-        entry.resource_id = entry._id;
+    return async function (hook) {
+        const resourceId = hook.result._id;
+        const existing = await hook.app.service('log')
+            .find({query: {resource_id: resourceId, $limit: 1, $sort: {since: -1}}});
+        if (existing.length > 0) {
+            const paths = ['tetra', 'callSign', 'type', 'state', 'lastPosition', 'destination', 'contact', 'info', 'hidden'];
+            const previous = _.pick(existing[0], paths);
+            const current = _.pick(hook.result, paths);
+            if (equal(previous, current)) {
+                // Do not create log entry if tracked properties did not change
+                return;
+            }
+        }
+        const entry = _.omit(hook.result, '_id');
+        entry.resource_id = resourceId;
         entry.user_id = _.get(hook, 'params.user._id');
-        delete entry._id;
         return hook.app.service('log').create(entry).then(() => hook);
     };
 };
