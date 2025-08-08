80e5a94c27c5514e1480cdc15a9c6207b96d346f
webansicht server <webansicht@bran.at>
2025-08-08 11:44:25 +0200

    fix: allow multi updates in messages and resources

--- src/services/messages/messages.service.js
+++ src/services/messages/messages.service.js
@@ -9,7 +9,8 @@ module.exports = function (app) {
 
   const options = {
     Model,
-    paginate
+    paginate,
+    multi: true
   };
 
   // Initialize our service with any options it requires
--- src/services/resources/resources.service.js
+++ src/services/resources/resources.service.js
@@ -8,7 +8,7 @@ module.exports = function () {
   const Model = createModel(app);
 
   // Initialize our service with any options it requires
-  app.use('/resources', createService({ Model }));
+  app.use('/resources', createService({ Model, multi: true }));
 
   // Get our initialized service so that we can register hooks and filters
   const service = app.service('resources');
