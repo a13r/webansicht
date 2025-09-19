c2251c2de279f51872037dc51138f3adcb713557
Andreas Bruckner <andreas@bruckner.cc>
2025-09-19 16:37:27 +0200

    chore: fix server initialization and logging setup (was broken after upgrade to Feathers v5)

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@
 const logger = require('winston');
 const app = require('./app');
 const port = app.get('port');
-const server = app.listen(port);
+app.listen(port).then(() => {
+    logger.info(`Feathers application started on port ${port}`);
+});
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
-);
-
-server.on('listening', () =>
-  logger.info(`Feathers application started on port ${port}`)
 );
